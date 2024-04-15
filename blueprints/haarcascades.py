6ec5d1faa54c33639229d6073680b76d121a639a
WaterNewt <134024849+WaterNewt@users.noreply.github.com>
2024-04-15 07:17:09 +0500

    Improved error handling

--- blueprints/haarcascades.py
+++ blueprints/haarcascades.py
@@ -1,13 +1,22 @@
 import json
-from flask import Blueprint, send_file, render_template
+from flask import Blueprint, send_file, request
 
 app = Blueprint("haarcascades", __name__)
 
 
-@app.route("/haarcascades/")
-@app.route('/haarcascades/<cascade_file>')
-def index(cascade_file=None):
-    if cascade_file is not None:
-        return send_file("haarcascades/"+cascade_file, as_attachment=True), 200
+@app.route('/haarcascades/')
+def index():
+    cascade_file = request.args.get("file", None, str)
+    print(cascade_file)
     with open("haarcascades.json", "r") as f:
-        return render_template("haarcascades.html", haarcascades=json.load(f)), 200
+        haarcascades = json.load(f)
+    available_files = [i.split("/")[1] for i in haarcascades.values()]
+    if cascade_file is not None:
+        if cascade_file in available_files:
+            try:
+                return send_file("haarcascades/"+cascade_file, as_attachment=True), 200
+            except FileNotFoundError:
+                return {"code": 404, "error": "Could not find cascade file"}, 404
+        else:
+            return {"code": 404, "error": "Invalid haarcascade file"}, 404
+    return haarcascades, 200
